4857f9163842245c85375fe39c7aa603f341a769
atnekadakan4444 <actandgodwillact12345@gmail.com>
2026-02-11 11:21:08 +0900

    ADD: ReactRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Signup from "./pages/Signup"
+import CreateWorkspace from "./pages/CreateWorkspace"
+import Signin from "./pages/Signin"
+import Home from "./pages/Home"
+
 function App() {
   return (
-    <>
-      <div>Hello World</div>
-    </>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<CreateWorkspace />} />
+          <Route path="/:workspaceId/:channelId" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
