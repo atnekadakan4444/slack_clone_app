b71280782def0251e90b6a8e632b8284de695a89
atnekadakan4444 <actandgodwillact12345@gmail.com>
2026-02-21 14:40:41 +0900

    ADD: create workspace

--- frontend/src/pages/CreateWorkspace/index.tsx
+++ frontend/src/pages/CreateWorkspace/index.tsx
@@ -1,16 +1,26 @@
-import '../Signup/auth.css';
-import CreateWorkspaceModal from '../Home/WorkspaceSelector/CreateWorkspaceModal';
-import { useCurrentUserStore } from '../../modules/auth/current-user.state';
-import { Navigate } from 'react-router-dom';
+import "../Signup/auth.css";
+import CreateWorkspaceModal from "../Home/WorkspaceSelector/CreateWorkspaceModal";
+import { useCurrentUserStore } from "../../modules/auth/current-user.state";
+import { Navigate } from "react-router-dom";
+import { workspaceRepository } from "../../modules/workspaces/workspace.repository";
 
 function CreateWorkspace() {
   const { currentUser } = useCurrentUserStore();
+
+  const createWorkspace = async (name: string) => {
+    try {
+      const newWorkspace = await workspaceRepository.create(name);
+      console.log("Created workspace:", newWorkspace);
+    } catch (error) {
+      console.error("ワークスペースの作成に失敗:", error);
+    }
+  };
 
   if (currentUser == null) return <Navigate to="/signin" />;
 
   return (
     <div>
-      <CreateWorkspaceModal />
+      <CreateWorkspaceModal onSubmit={createWorkspace} />
     </div>
   );
 }
