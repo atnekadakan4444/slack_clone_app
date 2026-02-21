be76cdcd9f30991c6227fcf5b62735142bfe383e
atnekadakan4444 <actandgodwillact12345@gmail.com>
2026-02-21 17:30:56 +0900

    ADD: redirect homeWS

--- frontend/src/modules/workspaces/workspace.entity.ts
+++ frontend/src/modules/workspaces/workspace.entity.ts
@@ -1,7 +1,12 @@
+import { Channel } from "../channels/channel.entity";
+
 export class Workspace {
-  id: number;
-  name: string;
+  id!: string;
+  name!: string;
+  channels!: Channel[];
+
   constructor(data: Workspace) {
     Object.assign(this, data);
+    this.channels = data.channels.map(channel => new Channel(channel));
   }
 }
