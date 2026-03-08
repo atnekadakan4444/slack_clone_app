131ab8faf59dc4d0485f91e828d37374358a5e56
atnekadakan4444 <actandgodwillact12345@gmail.com>
2026-03-08 19:01:14 +0900

    ADD: display profile icon

--- frontend/src/pages/Home/WorkspaceSelector/index.tsx
+++ frontend/src/pages/Home/WorkspaceSelector/index.tsx
@@ -21,7 +21,7 @@ function WorkspaceSelector(props: Props) {
     setShowProfileModal,
   } = useUiStore();
   const navigate = useNavigate();
-  const { setCurrentUser } = useCurrentUserStore();
+  const { currentUser, setCurrentUser } = useCurrentUserStore();
 
   const createWorkspace = async (name: string) => {
     try {
@@ -67,7 +67,7 @@ function WorkspaceSelector(props: Props) {
         >
           <img
             src={
-              "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+              currentUser?.iconUrl
             }
             alt="Posted image"
             className="message-image"
